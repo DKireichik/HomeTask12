ce158a54e3ca7296b0b0656806619f4dd361d834
Darya Kireychik <dsilchencko@gmail.com>
2023-11-13 12:09:51 +0300

    add uielements

---
+++ HomeTask12/12/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  12
+//
+//  Created by Darya on 10.11.23.
+//
+
+import UIKit
+
+
+class ViewController: UIViewController, ViewDelegate {
+
+    
+    let customView = View()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.addSubview(customView)
+        customView.translatesAutoresizingMaskIntoConstraints = false
+        customView.topAnchor.constraint(equalTo: view.topAnchor, constant: 80).isActive = true
+        customView.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 16).isActive = true
+        customView.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -16).isActive = true
+      
+    }
+    
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        print( customView.self.firstNameText)
+    }
+    
+    
+
+}
+
